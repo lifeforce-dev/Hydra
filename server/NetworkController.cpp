61bbef879737cdf6ccfda728b9b6790468552ebe
Joshua Sanders <joshua.sandersr@gmail.com>
2019-08-11 18:14:35 -0700

    Add main server files

---
+++ server/NetworkController.cpp
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------
+//
+// NetworkController.cpp
+//
+
+#include "server/NetworkController.h"
+
+namespace Server {
+
+//===============================================================================
+
+NetworkController::NetworkController()
+{
+
+}
+
+NetworkController::~NetworkController()
+{
+
+}
+
+//===============================================================================
+
+} // namespace Server
