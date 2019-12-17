a8d3a50e685f90255db11e4375075811b7c11146
Joshua Sanders <joshua.sandersr@gmail.com>
2019-12-16 23:30:14 -0800

    Adds SpellIdEnums.h
    SpellIds for every spell in the game will be added here.

---
+++ common/generated/SpellIdEnums.h
@@ -0,0 +1,23 @@
+//-------------------------------------------------------------------------------
+//
+// SpellIdEnums.h
+//
+
+#pragma once
+
+// This is a generated file! Any changes here will be lost!!!
+
+#include <cstdint>
+
+namespace Common {
+
+//===============================================================================
+
+enum struct SpellIds : uint32_t
+{
+    NONE = 0,
+};
+
+//===============================================================================
+
+} // namespace Common
